ade031c428ea7377016346a3d5155a621f0adc01
Tkwok7 <tkwok7@gatech.edu>
2023-11-18 00:34:06 -0500

    post request

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+import base64
+
 
 app = Flask(__name__)
 
@@ -8,19 +10,25 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    batch_json = {}
+    batch_json["images"] = {}
     if not request.files:
         return "No File"
     for filename in request.files:
-        file = request.files[filename]
         if filename != ' ':
-            return filename
+            file = request.files[filename]
+            file_content = file.read()
+            base64_content = base64.b64encode(file_content)
+            base64_content_string = base64_content.decode('utf-8')
+            batch_json["images"].g
+    return jsonify(batch_json)
 
 @app.route('/test')
 def test():
     return "API WORKED"
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(port=8001, debug=True)
 
 # def encode_image(image_path):
 #   with open(image_path, "rb") as image_file:
