494d0fab7a9e870f88caea4e6ab8e8398f3e4e63
msteele3 <mpsteele553@gmail.com>
2023-11-18 19:20:32 -0500

    finished profile page preliminary

--- front_end/symmetry/src/pages/ProfilePage.jsx
+++ front_end/symmetry/src/pages/ProfilePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { Pie } from 'react-chartjs-2';
 import { useLocation } from 'react-router-dom';
 import bSteeleImage from '../resources/bensteele.svg';
@@ -15,6 +16,7 @@ import DailyBreakDownComponent from '../components/DailyBreakdownComponent';
 
 const ProfilePage = () => {
   const location = useLocation();
+  const [selectedDay, setSelectedDay] = useState("M");
 
   const searchParams = new URLSearchParams(location.search);
   const username = searchParams.get('username');
@@ -26,16 +28,18 @@ const ProfilePage = () => {
   };
 
   const leftPanelStyle = {
-    flex: 1,
+    flex: 5,
     backgroundColor: colors.backgroundColor,
     flexDirection: 'column',
 
   };
 
   const rightPanelStyle = {
-    flex: 1,
+    flex: 4,
     backgroundColor: colors.backgroundColor,
     flexDirection: 'column',
+    fontFamily: "'Inria Sans', sans-serif",
+    color: "#FFFFFF",
 
   };
   const userImages = {
@@ -58,6 +62,13 @@ const ProfilePage = () => {
     ['Assisting Team Members', 9],
     ['Meetings', 12],
   ];
+  const focusedScore = {
+    "M": 78,
+    "T": 82,
+    "W": 20,
+    "TR": 76,
+    "F": 78
+  }
   const labels = mockTasks.map(task => task[0]);
   const dataPoints = mockTasks.map(task => task[1]);
   const data = {
@@ -86,25 +97,28 @@ const ProfilePage = () => {
     kTaylorImage,
     mMasonImage
   ];
-
   const bulletPointItems = [
     { color: '#FFD700', text: '12 Hours Debugging' },
     { color: '#8A2BE2', text: '2 Hours in Team Sessions' },
-    // ... add other bullet points as needed
+    // Assuming teal represents creative work
+    { color: '#00CED1', text: '4 Hours in Creative Design' },
+    // Assuming red represents administrative tasks
+    { color: '#FF4500', text: '3 Hours on Administrative Tasks' }
   ];
+  
 
   return (
     <div style={profileContainerStyle}>
       <div style={leftPanelStyle}>
-        <div style={{ marginLeft: "20px", marginTop: "20px" }}>
+        <div style={{ marginLeft: "20px", marginTop: "20px", transform: 'translateY(30px)'}}>
           <ProfileComponent profileSvg={selectedImage} name={selectedName} onClick={() => { }} />
           <div style={{ width: "70%", marginLeft: "5px" }}>
-            <WorkGradComponent value={104} backgroundColor={colors.backgroundColor} />
+            <WorkGradComponent value={focusedScore[selectedDay]} focusedWorkPercent={focusedScore[selectedDay]} backgroundColor={colors.backgroundColor} />
 
 
           </div>
           <div style={{ width: "85%", marginLeft: "5px"}}>
-            <DailyBreakDownComponent></DailyBreakDownComponent>
+            <DailyBreakDownComponent selectedDay={selectedDay} setSelectedDay={setSelectedDay}></DailyBreakDownComponent>
           </div>
 
         </div>
@@ -113,12 +127,14 @@ const ProfilePage = () => {
         {/* Left panel content */}
       </div>
       <div style={rightPanelStyle}>
-        {/* Right panel content */
-        <div style={{ width: '40%', height: '40%'}}>
-            <Pie data={data} options={chartOptions} />
-            <ColoredBullets items={bulletPointItems} />
+        <div style={{ width: '50%', height: '50%', marginLeft: "14vw"}}>
+         
+            <Pie style ={{transform: 'translateX(-80px)'}}data={data} options={chartOptions} />
+            <h2 style={{justifyContent: 'center', marginLeft: "10px",  transform: 'translateY(-40px) translateX(-90px)', fontSize: "1.7em"}}>This week, Tyler spent</h2>
+            <div style={{transform: 'translateY(-40px) translateX(-20px)'}}><ColoredBullets items={bulletPointItems} /></div>
+            
             </div>
-        }
+        
       </div>
     </div>
   );
