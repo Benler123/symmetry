dbb6d9d4eb450ea5d8ffbda36c73799891f7f331
Tkwok7 <tkwok7@gatech.edu>
2024-01-08 22:06:44 -0500

    llama works

--- Models/ImageClients/llama_vision_client.py
+++ Models/ImageClients/llama_vision_client.py
@@ -2,13 +2,41 @@ from Models.Image_Model import Image_Model
 import requests
 import os
 import re 
+import time
 import json
 from prompts import ACTIVITY_CAPTURE_PROMPT as prompt 
 
 class llama_vision_client(Image_Model):
-    def __init__():
-        pass
+    def __init__(self, BASE_URI='https://sbg69x9tdmmurm-5000.proxy.runpod.net', STREAM=False):
+        self.BASE_URI = BASE_URI
+
     def explain_images(self, base64_images):
-        raise NotImplementedError("This method is not implemented")
+        start_time = time.time()
+        image_texts = [self.explain_single_image(image) for image in base64_images] 
+        print(image_texts)
+        end_time = time.time()
+        print(f"Time taken: {end_time - start_time} seconds")
+        return 'this is a test'
+
+  
+
+
+    def explain_single_image(self, base64_image):
+        payload = {
+        'model_path': 'liuhaotian/llava-v1.5-7b',
+        'image_base64': base64_image,
+        'prompt': 'What is going on in this image',
+        'temperature': 0.2,
+        'max_new_tokens': 512,
+        'stream': False
+    }
+        
+        r = requests.post(
+        f'{self.BASE_URI}/inference',
+        json=payload,
+    )
+        # print(f'Status code: {r.status_code}')
     
-    
+        resp_json = r.json()
+        return resp_json['response']
+ 
