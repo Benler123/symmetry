11c51b6be9df83d3163c80b7f16b2bb4ee596b1d
Tkwok7 <tkwok7@gatech.edu>
2024-01-08 20:41:45 -0500

    setup dor llama

---
+++ Models/ImageClients/llama_vision_client.py
@@ -0,0 +1,14 @@
+from Models.Image_Model import Image_Model  
+import requests
+import os
+import re 
+import json
+from prompts import ACTIVITY_CAPTURE_PROMPT as prompt 
+
+class llaama_vision_client(Image_Model):
+    def __init__():
+        pass
+    def explain_images(self, base64_images):
+        raise NotImplementedError("This method is not implemented")
+    
+    
